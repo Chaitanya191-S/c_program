2f7a8bbb64150d925e51344a855851b8fd525cf9
Chaitanya191-S <sunilkejkar2679@gmail.com>
2025-11-07 23:28:43 +0530

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &num);
+
+    printf("Number   Cube\n");
+    for(i = 1; i <= num4; i++) {
+        printf("%d \t  %d\n", i, i*i*i);
+    }
+
+    return 0;
+}
